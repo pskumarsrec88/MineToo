0f31511d407a85f141311a84bf30354e9b628498
saravanakumar <pskumarsrec88@gmail.com>
2012-05-30 14:19:29 +0530

    2nd

--- app/mailers/sendmail.rb
+++ app/mailers/sendmail.rb
@@ -1,7 +1,7 @@
 class Sendmail < ActionMailer::Base
   default :from => "pskcoolking@gmail.com"
   def tellfriend(contact)
-    url="http://localhost:3000/home"
+    url="http://minetoorails.heroku.com/"
     #~ @friend_name=fn
     #~ @friend_email=fe
     mail(:to=>contact[:email], :subject=>"Welcome to Minetoo", :body=>"Your Friend "+contact[:your_name]+" ["+contact[:your_name]+"]"+" invites you to MineToo. Click the link to enter the application "+url)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,21 @@ Devise::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => 'minetoorails.heroku.com' }
+  
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+:address => "smtp.gmail.com" ,
+:port => 25,
+:domain => "gmail.com",
+:authentication => :plain,
+:user_name => "pskcoolking@gmail.com" ,
+:password => "51425162"
+}
+
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+  
 end
