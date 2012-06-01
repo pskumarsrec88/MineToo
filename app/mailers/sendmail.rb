8567a3d15f19458ad78ec0f317def3398584899c
saravanakumar <pskumarsrec88@gmail.com>
2012-06-01 18:04:04 +0530

    validation

--- app/mailers/sendmail.rb
+++ app/mailers/sendmail.rb
@@ -2,7 +2,7 @@ class Sendmail < ActionMailer::Base
   default :from => "pskcoolking@gmail.com"
   def tellfriend(contact)
     url="http://minetoorails.heroku.com/"
-    mail(:to=>contact[:email], :subject=>"Welcome to Minetoo", :body=>"Your Friend "+contact[:your_name]+" ["+contact[:your_name]+"]"+" invites you to MineToo. Click the link to enter the application "+url)
+    mail(:to=>contact[:email], :subject=>"Welcome to Minetoo", :body=>"Your Friend "+contact[:your_name]+" ["+contact[:your_email]+"]"+" invites you to MineToo. Click the link to enter the application "+url)
   end
   
   def contactadmin(contact)
